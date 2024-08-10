177f21cbcdb2a8d0896ce4c3dd9a6b64f224176a
caio deiro <caioguimaraes12@outlook.com>
2024-08-10 11:18:48 -0300

    uploud base changes of code

---
+++ lib/src/app/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:bento_layout_challenger/src/app/home/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
